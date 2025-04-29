26fffe40aebc15868402d62d3fc89a0c6a9ca106
QuarkPixel <m2462864403@gmail.com>
2025-04-29 16:38:18 +0800

    feat: push() no more need T: Copy + Copy

--- src/traits.rs
+++ src/traits.rs
@@ -1,22 +1,29 @@
 use crate::ChunkedVec;
 
 impl<T, const N: usize> ChunkedVec<T, N> {
-    pub fn push(&mut self, value: T)
-    where
-        T: Default + Copy,
-    {
+    pub fn push(&mut self, value: T) {
         let chunk_idx = self.len / N;
         let offset = self.len % N;
 
         if chunk_idx >= self.data.len() {
             assert_eq!(offset, 0);
-            let mut new_chunk = Box::new([T::default(); N]);
-            new_chunk[0] = value;
-            self.data.push(new_chunk);
+            let chunk = Self::create_new_chunk(value);
+            self.data.push(chunk);
         } else {
             self.data[chunk_idx][offset] = value;
         }
         self.len += 1;
+    }
+
+    fn create_new_chunk(value: T) -> Box<[T; N]> {
+        let mut chunk = Box::new_uninit_slice(N);
+        // Initialize the first element
+        chunk[0].write(value);
+        // Convert to Box<[T; N]>
+        unsafe {
+            let ptr = Box::into_raw(chunk) as *mut [T; N];
+            Box::from_raw(ptr)
+        }
     }
 
     #[inline]
